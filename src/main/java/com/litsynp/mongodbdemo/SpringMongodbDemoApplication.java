76116dd0cd79631bc2b492350051237596a65fd6
litsynp <nocte55is@gmail.com>
2022-06-20 19:23:01 +0900

    build: init project

---
+++ src/main/java/com/litsynp/mongodbdemo/SpringMongodbDemoApplication.java
@@ -0,0 +1,12 @@
+package com.litsynp.mongodbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMongodbDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/litsynp/mongodbdemo/SpringMongodbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litsynp.mongodbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
