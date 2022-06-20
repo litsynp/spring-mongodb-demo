7fac00c29dd8133132c363fcf384f6264a34935a
litsynp <nocte55is@gmail.com>
2022-06-20 19:45:10 +0900

    refactor: rename main class

--- src/main/java/com/litsynp/mongodbdemo/MongoApplication.java
+++ src/main/java/com/litsynp/mongodbdemo/MongoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMongodbDemoApplication {
+public class MongoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringMongodbDemoApplication.class, args);
+        SpringApplication.run(MongoApplication.class, args);
     }
 }
